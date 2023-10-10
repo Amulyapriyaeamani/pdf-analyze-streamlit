ee4868dc0c4b4d2678de7522f5a0c5f2944e47db
Amulyapriyaeamani <145454104+Amulyapriyaeamani@users.noreply.github.com>
2023-10-10 21:46:34 +0530

    Update qa_app.py

--- qa_app.py
+++ qa_app.py
@@ -120,7 +120,7 @@ def main():
         padding: 0px 0px;
         text-align: center;
     ">
-        <p>Made by <a href='https://twitter.com/mehmet_ba7'>Mehmet Balioglu</a></p>
+        <p>Made by <a href='https://x.com/amulyaeamani'>The Tech Wizzes</a></p>
     </div>
     """
 
@@ -175,7 +175,7 @@ def main():
         """,
         unsafe_allow_html=True,
     )
-    st.sidebar.image("img/logo1.png")
+    st.sidebar.image("img/PdfBot.png")
 
 
    
@@ -265,7 +265,7 @@ def main():
         # Check if there are no generated question-answer pairs in the session state
         if 'eval_set' not in st.session_state:
             # Use the generate_eval function to generate question-answer pairs
-            num_eval_questions = 10  # Number of question-answer pairs to generate
+            num_eval_questions = 5  # Number of question-answer pairs to generate
             st.session_state.eval_set = generate_eval(
                 loaded_text, num_eval_questions, 3000)
 
@@ -275,8 +275,8 @@ def main():
                 f"""
                 <div class="css-card">
                 <span class="card-tag">Question {i + 1}</span>
-                    <p style="font-size: 12px;">{qa_pair['question']}</p>
-                    <p style="font-size: 12px;">{qa_pair['answer']}</p>
+                    <p style="font-size: 12px; font-color: #000000;">{qa_pair['question']}</p>
+                    <p style="font-size: 12px; font-color: #000000;">{qa_pair['answer']}</p>
                 </div>
                 """,
                 unsafe_allow_html=True,
