d4d0c06a32fb12cba461a92fd56436cf4054db92
Amulyapriyaeamani <145454104+Amulyapriyaeamani@users.noreply.github.com>
2023-10-11 11:32:36 +0530

    Update qa_app.py

--- qa_app.py
+++ qa_app.py
@@ -17,7 +17,7 @@ from langchain.callbacks.base import CallbackManager
 from langchain.embeddings import HuggingFaceEmbeddings
 
 
-st.set_page_config(page_title="PDF Analyzer",page_icon=':shark:')
+st.set_page_config(page_title="PDF Analyzer",page_icon=':page_facing_up:')
 
 @st.cache_data
 def load_docs(files):
