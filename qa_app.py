f449293fb55d78e3a02ba253a38fb7ec0313c5ff
Amulyapriyaeamani <145454104+Amulyapriyaeamani@users.noreply.github.com>
2023-10-11 11:36:38 +0530

    Update qa_app.py

--- qa_app.py
+++ qa_app.py
@@ -17,7 +17,7 @@ from langchain.callbacks.base import CallbackManager
 from langchain.embeddings import HuggingFaceEmbeddings
 
 
-st.set_page_config(page_title="PDF Analyzer",page_icon=':page_facing_up:')
+st.set_page_config(page_title="PdfBot",page_icon=':page_facing_up:')
 
 @st.cache_data
 def load_docs(files):
@@ -120,7 +120,7 @@ def main():
         padding: 0px 0px;
         text-align: center;
     ">
-        <p>Made by <a href='https://x.com/amulyaeamani'>The Tech Wizzes</a></p>
+        <p>Made by <a href='https://x.com/amulyaeamani'>The Tech Whizzes</a></p>
     </div>
     """
 
